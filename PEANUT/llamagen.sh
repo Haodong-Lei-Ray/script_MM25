c2081b04b6f1832dc26a1d00220747b2a1999a2b
Haodong <220246386@edu.seu.cn>
2025-04-06 17:29:28 +0800

    new

---
+++ PEANUT/llamagen.sh
@@ -0,0 +1,7 @@
+cd /home/leihaodong/MM25/PEANUT
+python main.py generate_images \
+ --model llamagen \
+ --model_type eagle \
+ --model_path  jadohu/LlamaGen-T2I \
+ --drafter_path  jadohu/llamagen_drafter \
+ --output_dir /home/leihaodong/MM25/script_MM/PEANUT/exp/llamagen \
